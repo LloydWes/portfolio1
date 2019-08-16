7e78ce6a8f7d814735a15de2b9504bc6932bedd7
Lloyd <lloydwestbury@gmail.com>
2019-08-16 18:49:22 +0200

    changed cv button size for small screen

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function(){
 	console.log('script has been loaded');
 	let menu = document.querySelector('#ham_menu');
 	menu.addEventListener('click', function(e){
+		console.log('Event');
 		let bars = document.querySelector('#ham_menu');
 		bars.classList.toggle('change');
 		document.querySelector('#ham_list').classList.toggle('change');
