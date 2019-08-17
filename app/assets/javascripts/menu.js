e2d4ba486b8363284d61ae3f3ec88fe476b9e0d7
Lloyd <lloydwestbury@gmail.com>
2019-08-17 12:11:45 +0200

    added proper tag for menu.js

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -3,8 +3,11 @@ window.addEventListener('load', function(){
 	let menu = document.querySelector('#ham_menu');
 	menu.addEventListener('click', function(e){
 		console.log('Event');
-		let bars = document.querySelector('#ham_menu');
-		bars.classList.toggle('change');
-		document.querySelector('#ham_list').classList.toggle('change');
+		// let ham_menu = document.querySelector('#ham_menu');
+		console.log(menu);
+		menu.classList.toggle('change');
+		let ham_list = document.querySelector('#ham_list');
+		console.log(ham_list);
+		ham_list.classList.toggle('change');
 	});
 });
