30ac1b610bc344d42180e43335b6169ca191b32a
Lloyd <lloydwestbury@gmail.com>
2019-08-21 13:57:36 +0200

    mend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :add_www_subdomain
+
+  private
+  def add_www_subdomain
+    unless /^www/.match(request.host)
+      # redirect_to("#{request.protocol}x.com#{request.request_uri}",
+      # :status => 301)
+      redirect_to("#{request.protocol}www.#{request.host_with_port}",status: 301)
+
+    end
+  end
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,4 @@
 class StaticController < ApplicationController
-  before_filter :add_www_subdomain
   def a_propos
 
   end
@@ -9,11 +8,4 @@ class StaticController < ApplicationController
 
   end
 
-  private
-  def add_www_subdomain
-    unless /^www/.match(request.host)
-      redirect_to("#{request.protocol}x.com#{request.request_uri}",
-        :status => 301)
-    end
-  end
 end
